a984b33f1abbc944f5c5ee01152aae3f581f5bc5
Noah Too <krivahtoo@gmail.com>
2022-09-30 18:27:10 +0300

    override the default 404 handler

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Slim\App;
 use PDO;
+use Slim\Http\Request;
+use Slim\Http\Response;
 
 $settings = require __DIR__ . '/../app/settings.php';
 $routes = require __DIR__ . '/../app/routes.php';
@@ -40,5 +42,22 @@ $container['db'] = function ($c) {
 };
 
 $routes($app);
+
+// Override the default Not Found Handler
+// This should return index.html for the frontend to handle the routing
+unset($app->getContainer()['notFoundHandler']);
+$app->getContainer()['notFoundHandler'] = function ($c) {
+  return function (Request $request, Response $response) use ($c) {
+    $c->logger->debug('404 error');
+    if ($request->isGet()) {
+      return $response
+        ->withHeader('Content-Type', 'text/html')
+        ->write(file_get_contents(__DIR__ . '/index.html'));
+    } else {
+      return $response
+        ->withJson(['error' => 'Not found'], 404);
+    }
+  };
+};
 
 $app->run();
