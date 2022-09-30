1b04ee2729d63e6fadf184a50a8fbcf40359ad88
Noah Too <krivahtoo@gmail.com>
2022-09-30 14:44:49 +0300

    Setup vue 3 frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  clearScreen: false,
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: '../public'
+  }
+})
