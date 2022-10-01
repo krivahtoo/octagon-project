e2044f3aae2a976057b89dd303141a3c474caf48
Noah Too <krivahtoo@gmail.com>
2022-10-01 22:24:14 +0300

    lsp formatting

--- src/Auth/Token.php
+++ src/Auth/Token.php
@@ -7,50 +7,52 @@ use App\ResponseBridge;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use OAuth2;
+use OAuth2\Response;
 
 /**
  * Slim route for /token endpoint.
  */
 class Token
 {
-    const ROUTE = '/token';
+  const ROUTE = '/token';
 
-    /**
-     * The OAuth2 server instance.
-     *
-     * @var OAuth2\Server
-     */
-    private $server;
+  /**
+   * The OAuth2 server instance.
+   *
+   * @var OAuth2\Server
+   */
+  private $server;
 
-    /**
-     * Create a new instance of the Token route.
-     *
-     * @param OAuth2\Server $server The oauth2 server imstance.
-     */
-    public function __construct(OAuth2\Server $server)
-    {
-        $this->server = $server;
+  /**
+   * Create a new instance of the Token route.
+   *
+   * @param OAuth2\Server $server The oauth2 server imstance.
+   */
+  public function __construct(OAuth2\Server $server)
+  {
+    $this->server = $server;
+  }
+
+  /**
+   * Invoke this route callback.
+   *
+   * @param ServerRequestInterface $request   Represents the current HTTP request.
+   * @param ResponseInterface      $response  Represents the current HTTP response.
+   * @param array                  $arguments Values for the current route’s named placeholders.
+   *
+   * @return RequestInterface
+   */
+  public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $arguments = [])
+  {
+    $response = new Response();
+    $this->server->grantAccessToken(RequestBridge::toOAuth2($request), $response);
+
+    $response = ResponseBridge::fromOauth2($response);
+
+    if ($response->hasHeader('Content-Type')) {
+      return $response;
     }
 
-    /**
-     * Invoke this route callback.
-     *
-     * @param ServerRequestInterface $request   Represents the current HTTP request.
-     * @param ResponseInterface      $response  Represents the current HTTP response.
-     * @param array                  $arguments Values for the current route’s named placeholders.
-     *
-     * @return RequestInterface
-     */
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $arguments = [])
-    {
-        $response = ResponseBridge::fromOAuth2(
-            $this->server->handleTokenRequest(RequestBridge::toOAuth2($request))
-        );
-
-        if ($response->hasHeader('Content-Type')) {
-            return $response;
-        }
-
-        return $response->withHeader('Content-Type', 'application/json');
-    }
+    return $response->withHeader('Content-Type', 'application/json');
+  }
 }
