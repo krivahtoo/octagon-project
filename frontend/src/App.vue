2d4c40cfa30d40f1e08179dca7436b14cd5cd94c
Noah Too <krivahtoo@gmail.com>
2022-10-02 18:00:22 +0300

    restore token from localStorage

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,36 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
-import { useLoginStore } from '@/stores'
+import { useLoginStore, useUserStore } from '@/stores'
+import { onMounted, onBeforeMount } from 'vue'
 
-const login = useLoginStore()
+const store = useLoginStore()
+const user = useUserStore()
+
+onBeforeMount(async () => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    store.token = token
+  }
+  if (store.token !== '') {
+    const response = await fetch(`${store.apiServer}/api/user`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    if (response.ok) {
+      const res = await response.json()
+      console.log(res)
+      user.setUser(res)
+    }
+  }
+})
+
 </script>
 
 <template>
-  <NavBar v-if="login.isLoggedIn"/>
+  <NavBar v-if="store.isLoggedIn"/>
 
   <RouterView />
 </template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -15,7 +15,7 @@ import { RouterLink } from 'vue-router'
         <div class="text-center text-white px-6 md:px-12">
           <h1 class="text-5xl font-bold mt-0 mb-6">Hello</h1>
           <h3 class="text-3xl font-bold mb-8">Welcome</h3>
-          <RouterLink to="/login"
+          <RouterLink to="/profile"
             class="inline-block px-6 py-2.5 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
             data-mdb-ripple="true" data-mdb-ripple-color="light">
             View Profile
